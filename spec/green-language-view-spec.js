553029524c1019b89d3a394fb195f8b9a4674838
Christian <chris.heyn.dd@gmail.com>
2018-08-02 21:48:27 +0200

    init repo

---
+++ spec/green-language-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GreenLanguageView from '../lib/green-language-view';
+
+describe('GreenLanguageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
